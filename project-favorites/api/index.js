6ee2f0f393f1cb510df2d7a0ab28441f1a7b9382
Felipe-dSantos <lipehh18@gmail.com>
2023-07-13 00:35:52 -0300

    Update style

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -1,37 +1,37 @@
 const http = require('http')
-const data = require('./urls.json')
 const URL = require('url')
 const fs = require('fs')
 const path = require('path')
-
+const data = require('./urls.json')
 
 http.createServer((req, res) => {
 
-    const { name, url, del } = URL.parse(req.url, true).query //
+    const { name, url, del } = URL.parse(req.url, true).query
 
     res.writeHead(200, {
-        'Access-Control-Allow-Origin':'*'
+        'Access-Control-Allow-Origin': '*'
     })
-    
-    if (!name || !url)//se nome = false ou url = false executa o return
-        return res.end(JSON.stringify(data)) 
-        
-    if (del){
-        data.urls = data.urls.filter(item => item.url != url)
-        return writefile(message => res.end(message))
-    }
 
-    data.urls.push({name, url})
-
-    return writefile(message => res.end(message))
-    
-    function writefile() {
+    function writeFile(cb) {
         fs.writeFile(
-        path.join(__dirname, 'urls.json'),
-        JSON.stringify(data,null, 2),
-        err => {
-            if (err) throw err
-            res.end('operação realizada com sucesso!')
-        })
+            path.join(__dirname, 'urls.json'),
+            JSON.stringify(data, null, 2),
+            err => {
+                if (err) throw err
+                cb('Operação realizada com sucesso!')
+            }
+        )
     }
-}).listen(3000, () => console.log('API rodando'));
+
+    if(!name || !url)
+        return res.end(JSON.stringify(data))
+    
+    if(del){
+        data.urls = data.urls.filter(item => item.url != url)
+        return writeFile(message => res.end(message))
+    }
+    
+    data.urls.push({name,url})
+    return writeFile(message => res.end(message))
+
+}).listen(3000, () => console.log('API rodando...'))
